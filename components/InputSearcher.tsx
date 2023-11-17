f8bd0b40d344d07a7aead96a93d8fc377bedbaae
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-11-16 23:44:11 -0300

    feat: add components project

---
+++ components/InputSearcher.tsx
@@ -0,0 +1,13 @@
+
+export default function InputSearcher() {
+
+    return (
+        <div className="gap-8 mt-9 w-8/12 h-16 bg-gray-100 flex justify-between items-center p-3 rounded-sm">
+            <input className="text-base font-bold border-transparent bg-transparent focus:border-transparent focus:outline-none" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" stroke="#A745EC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M17.5 17.5L13.9167 13.9167" stroke="#A546ED" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        </div>
+    )
+}
---
+++ components/PageColors.tsx
@@ -0,0 +1,27 @@
+
+export default function PageColors () {
+    return (
+        <div className="flex w-4/5 justify-between" >
+
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" fill="none">
+                    <path d="M7 34.125V7.875C7 6.71468 7.46094 5.60188 8.28141 4.78141C9.10188 3.96094 10.2147 3.5 11.375 3.5H35V38.5H11.375C10.2147 38.5 9.10188 38.0391 8.28141 37.2186C7.46094 36.3981 7 35.2853 7 34.125ZM7 34.125C7 32.9647 7.46094 31.8519 8.28141 31.0314C9.10188 30.2109 10.2147 29.75 11.375 29.75H35" stroke="#999999" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+            </div>
+
+            <div className="flex justify-center items-center space-x-4">
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="52" height="24" viewBox="0 0 52 24" fill="none">
+                    <rect width="52" height="24" rx="12" fill="#747474" />
+                    <circle cx="12" cy="12" r="9" fill="white" />
+                </svg>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+                    <path d="M15 3.75C13.5082 5.24184 12.6701 7.26522 12.6701 9.375C12.6701 11.4848 13.5082 13.5082 15 15C16.4918 16.4918 18.5152 17.33 20.625 17.33C22.7348 17.33 24.7582 16.4918 26.25 15C26.25 17.225 25.5902 19.4001 24.354 21.2502C23.1179 23.1002 21.3609 24.5422 19.3052 25.3936C17.2495 26.2451 14.9875 26.4679 12.8052 26.0338C10.623 25.5997 8.61839 24.5283 7.04505 22.9549C5.47171 21.3816 4.40025 19.3771 3.96617 17.1948C3.53209 15.0125 3.75487 12.7505 4.60636 10.6948C5.45785 8.63914 6.89979 6.88213 8.74984 5.64597C10.5999 4.4098 12.775 3.75 15 3.75Z" stroke="#999999" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+
+            </div>
+
+        </div>
+    )
+}
